7da9ea24570860271dbca27792449aa40f227f71
Gludovatz Attila <attila@gludovatz.hu>
2022-02-19 21:57:21 +0100

    DB Access 1. resz

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\DB;
 
 class PostsController extends Controller
 {
-    public function show($post)
+    public function show($slug)
     {
         // return "my post";
 
         // return $post;
 
+        $post = \DB::table('posts')->where('slug', $slug)->first();
+        // dd($post);
         return view('post', compact('post'));
     }
 }
